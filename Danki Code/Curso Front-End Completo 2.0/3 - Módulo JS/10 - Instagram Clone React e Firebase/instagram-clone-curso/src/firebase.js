66abea29a553b6862e3d532a9ccf4c573a3cb43c
SilvaFili <filipe.f.silva@kaefer.com>
2024-07-19 13:48:38 -0300

    react

---
+++ Danki Code/Curso Front-End Completo 2.0/3 - Módulo JS/10 - Instagram Clone React e Firebase/instagram-clone-curso/src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyB4SPcX39EjWeJXO3ctyXa7DwJK31VHwM0",
+  authDomain: "instagram-clone-curso-769d6.firebaseapp.com",
+  projectId: "instagram-clone-curso-769d6",
+  storageBucket: "instagram-clone-curso-769d6.appspot.com",
+  messagingSenderId: "518395032335",
+  appId: "1:518395032335:web:8770a20e57cfe107e2447a",
+  measurementId: "G-50F47E4TCC",
+});
+
+firebase.initializeApp(firebaseApp);
+
+// Initialize Firebase
+const db = firebase.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+const functions = firebase.functions();
+
+export { db, auth, storage, functions };
